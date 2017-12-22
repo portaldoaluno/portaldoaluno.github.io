54f9495910c5efaeaed9cdc41471af7eff29d1c1
mntabacaria <patricklopes33@gmail.com>
2017-12-22 18:31:01 -0200

    First

---
+++ js/links.js
@@ -0,0 +1,210 @@
+function enem1() {
+	window.open ("vest/enemg_2016.pdf");
+	window.open ("vest/enem_2016.pdf");
+}
+
+function enem2() {
+	window.open ("vest/enemg2_2016.pdf");
+	window.open ("vest/enem2_2016.pdf");
+}
+
+function enem3() {
+	window.open ("vest/enemg_2015.pdf");
+	window.open ("vest/enem_2015.pdf");
+}
+
+function enem4() {
+	window.open ("vest/enemg2_2015.pdf");
+	window.open ("vest/enem2_2015.pdf");
+}
+
+function enem5() {
+	window.open ("vest/enemg_2014.pdf");
+	window.open ("vest/enem_2014.pdf");
+}
+
+function enem6() {
+	window.open ("vest/enemg2_2014.pdf");
+	window.open ("vest/enem2_2014.pdf");
+}
+
+function enem7() {
+	window.open ("vest/enemg_2013.pdf");
+	window.open ("vest/enem_2013.pdf");
+}
+
+function enem8() {
+	window.open ("vest/enemg2_2013.pdf");
+	window.open ("vest/enem2_2013.pdf");
+}
+
+function enem9() {
+	window.open ("vest/enemg_2012.pdf");
+	window.open ("vest/enem_2012.pdf");
+}
+
+function enem10() {
+	window.open ("vest/enemg2_2012.pdf");
+	window.open ("vest/enem2_2012.pdf");
+}
+
+function enem11() {
+	window.open ("vest/enemg_2011.pdf");
+	window.open ("vest/enem_2011.pdf");
+}
+
+function enem12() {
+	window.open ("vest/enemg2_2011.pdf");
+	window.open ("vest/enem2_2011.pdf");
+}
+
+function enem13() {
+	window.open ("vest/enemg_2010.pdf");
+	window.open ("vest/enem_2010.pdf");
+}
+
+function enem14() {
+	window.open ("vest/enemg2_2010.pdf");
+	window.open ("vest/enem2_2010.pdf");
+}
+
+function fisica1() {
+	window.open ("vest/gabarito_2017.pdf");
+	window.open ("vest/fisica_2017.pdf");
+}
+function fisica2() {
+	window.open ("vest/gabarito_2016.pdf");
+	window.open ("vest/fisica_2016.pdf");
+}
+function fisica3() {
+	window.open ("vest/gabarito_2015.pdf");
+	window.open ("vest/fisica_2015.pdf");
+}
+function fisica4() {
+	window.open ("vest/gabarito_2014.pdf");
+	window.open ("vest/fisica_2014.pdf");
+}
+function fisica5() {
+	window.open ("vest/gabarito_2013.pdf");
+	window.open ("vest/fisica_2013.pdf");
+}
+function fisica6() {
+	window.open ("vest/gabarito_2012.pdf");
+	window.open ("vest/fisica_2012.pdf");
+}
+function fisica7() {
+	window.open ("vest/gabarito_2011.pdf");
+	window.open ("vest/fisica_2011.pdf");
+}
+function quimica1() {
+	window.open ("vest/gabarito_2017.pdf");
+	window.open ("vest/quimica_2017.pdf");
+}
+function quimica2() {
+	window.open ("vest/gabarito_2016.pdf");
+	window.open ("vest/quimica_2016.pdf");
+}
+function quimica3() {
+	window.open ("vest/gabarito_2015.pdf");
+	window.open ("vest/quimica_2015.pdf");
+}
+function quimica4() {
+	window.open ("vest/gabarito_2014.pdf");
+	window.open ("vest/quimica_2014.pdf");
+}
+function quimica5() {
+	window.open ("vest/gabarito_2013.pdf");
+	window.open ("vest/quimica_2013.pdf");
+}
+function quimica6() {
+	window.open ("vest/gabarito_2012.pdf");
+	window.open ("vest/quimica_2012.pdf");
+}
+function quimica7() {
+	window.open ("vest/gabarito_2011.pdf");
+	window.open ("vest/quimica_2011.pdf");
+}
+function portugues1() {
+	window.open ("vest/gabarito_2017.pdf");
+	window.open ("vest/portugues_2017.pdf");
+}
+function portugues2() {
+	window.open ("vest/gabarito_2016.pdf");
+	window.open ("vest/portugues_2016.pdf");
+}
+function portugues3() {
+	window.open ("vest/gabarito_2015.pdf");
+	window.open ("vest/portugues_2015.pdf");
+}
+function portugues4() {
+	window.open ("vest/gabarito_2014.pdf");
+	window.open ("vest/portugues_2014.pdf");
+}
+function portugues5() {
+	window.open ("vest/gabarito_2013.pdf");
+	window.open ("vest/portugues_2013.pdf");
+}
+function portugues6() {
+	window.open ("vest/gabarito_2012.pdf");
+	window.open ("vest/portugues_2012.pdf");
+}
+function portugues7() {
+	window.open ("vest/gabarito_2011.pdf");
+	window.open ("vest/portugues_2011.pdf");
+}
+function ingles1() {
+	window.open ("vest/gabarito_2017.pdf");
+	window.open ("vest/ingles_2017.pdf");
+}
+function ingles2() {
+	window.open ("vest/gabarito_2016.pdf");
+	window.open ("vest/ingles_2016.pdf");
+}
+function ingles3() {
+	window.open ("vest/gabarito_2015.pdf");
+	window.open ("vest/ingles_2015.pdf");
+}
+function ingles4() {
+	window.open ("vest/gabarito_2014.pdf");
+	window.open ("vest/ingles_2014.pdf");
+}
+function ingles5() {
+	window.open ("vest/gabarito_2013.pdf");
+	window.open ("vest/ingles_2013.pdf");
+}
+function ingles6() {
+	window.open ("vest/gabarito_2012.pdf");
+	window.open ("vest/ingles_2012.pdf");
+}
+function ingles7() {
+	window.open ("vest/gabarito_2011.pdf");
+	window.open ("vest/ingles_2011.pdf");
+}
+function fuvest1() {
+	window.open ("vest/fuvestg_2016.pdf");
+	window.open ("vest/fuvest_2016.pdf");
+}
+function fuvest2() {
+	window.open ("vest/fuvestg_2015.pdf");
+	window.open ("vest/fuvest_2015.pdf");
+}
+function fuvest3() {
+	window.open ("vest/fuvestg_2014.pdf");
+	window.open ("vest/fuvest_2014.pdf");
+}
+function fuvest4() {
+	window.open ("vest/fuvestg_2013.pdf");
+	window.open ("vest/fuvest_2013.pdf");
+}
+function fuvest5() {
+	window.open ("vest/fuvestg_2012.pdf");
+	window.open ("vest/fuvest_2012.pdf");
+}
+function fuvest6() {
+	window.open ("vest/fuvestg_2011.pdf");
+	window.open ("vest/fuvest_2011.pdf");
+}
+function fuvest7() {
+	window.open ("vest/fuvestg_2010.pdf");
+	window.open ("vest/fuvest_2010.pdf");
+}
---
+++ js/simulados.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$(".dormir_box").hide();
+	$(".dormir").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow")
+	});
+});
+
+$(document).ready(function(){
+	$(".simulados_box").hide();
+	$(".simuladosi").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow")
+	});
+});
+
+$(document).ready(function(){
+	$(".metas_box").hide();
+	$(".metas").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow")
+	});
+});
+
+$(document).ready(function(){
+	$(".organizacao_box").hide();
+	$(".organizacao").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow")
+	});
+});
+
+$(document).ready(function(){
+	$(".habito_box").hide();
+	$(".habito").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow")
+	});
+});
+
+$(document).ready(function(){
+	$(".foco_box").hide();
+	$(".foco").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow")
+	});
+});
+
